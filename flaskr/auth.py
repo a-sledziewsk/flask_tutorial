69dc4e239f74a1676ffd33eecc7a65e19e61d338
Adam Sledziewski <a.sledziewsk@samsung.local>
2018-08-09 14:50:38 +0200

    Website in progress

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,9 +1,8 @@
 import functools
 
-from flask import(
-    Blueprint, flash, g, redirect, render_template, request, session,
-    url_for)
-
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
@@ -21,18 +20,18 @@ def register():
 
         if not username:
             error = 'Username is required.'
-
         elif not password:
-            error = 'Password is required'
-
-        elif db.execute('SELECT id FROM user WHERE username = ?', (username,)
-                        ).fetchone() is not None:
-            error = "User {} is already registered".format(username)
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
 
         if error is None:
-            db.execute('INSERT INTO user (username, password) VALUES (?, ?)'),
-            (username, generate_password_hash(password))
-
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
             db.commit()
             return redirect(url_for('auth.login'))
 
@@ -41,17 +40,16 @@ def register():
     return render_template('auth/register.html')
 
 
-bp.route('/login', methods=('GET', 'POST'))
-
-
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        password = request.fomr['password']
+        password = request.form['password']
         db = get_db()
         error = None
-        user = db.execute('SELECT user WHERE username ?',
-                          (username).fetchone())
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
 
         if user is None:
             error = 'Incorrect username.'
@@ -60,7 +58,7 @@ def login():
 
         if error is None:
             session.clear()
-            session['user_id'] = user_id['id']
+            session['user_id'] = user['id']
             return redirect(url_for('index'))
 
         flash(error)
@@ -69,14 +67,15 @@ def login():
 
 
 @bp.before_app_request
-def load_logged_in_use():
+def load_logged_in_user():
     user_id = session.get('user_id')
 
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute('SELECT * FROM user WHERE id = ?',
-                                  (user_id,)).fetchone()
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
 
 
 @bp.route('/logout')
--- flaskr/db.py
+++ flaskr/db.py
@@ -8,7 +8,8 @@ def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES)
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
         g.db.row_factory = sqlite3.Row
 
     return g.db
@@ -36,6 +37,9 @@ def init_db_command():
     click.echo('Initialized the database.')
 
 
+
+
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
+
